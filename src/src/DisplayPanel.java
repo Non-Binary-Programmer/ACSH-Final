f1bd48a819fe9dc377458378eccce2be567b409d
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-05-06 07:42:33 -0700

    Made a grid appear.

---
+++ src/src/DisplayPanel.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DisplayPanel extends JPanel {
+    public static final int PX_WIDTH = 800;
+    public static final int PX_HEIGHT = 800;
+
+    private int gridWidth, gridHeight, pxMargin;
+
+    public DisplayPanel () {
+        setBackground(Color.BLACK);
+
+        // addInputListener(controller);
+
+        gridWidth = 8;
+        gridHeight = 8;
+        pxMargin = 10;
+    }
+
+    protected void paintComponent (Graphics g) {
+        super.paintComponent(g);
+
+        int widthPerCell = (int) Math.ceil((double) PX_WIDTH / gridWidth);
+        int heightPerCell = (int) Math.ceil((double) PX_HEIGHT / gridHeight);
+
+        g.setColor(Color.WHITE);
+
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++) {
+                g.fillRect((widthPerCell * x) + pxMargin, (heightPerCell * y) + pxMargin,
+                        widthPerCell - (2 * pxMargin), heightPerCell - (2 * pxMargin));
+            }
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(PX_WIDTH, PX_HEIGHT);
+    }
+}
---
+++ src/src/GameDisplay.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class GameDisplay {
+    public static final String GAME_NAME = "Dodge!";
+
+    private JFrame frame;
+    private DisplayPanel dPanel;
+
+    public GameDisplay() {
+        frame = new JFrame(GAME_NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        dPanel = new DisplayPanel();
+        frame.add(dPanel);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
