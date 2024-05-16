0f7b9e3c7d1e4d4eb2872d76f6f2225c6938e929
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-05-16 14:32:55 -0700

    made leaderboard even better.

--- src/src/LeaderboardPanel.java
+++ src/src/LeaderboardPanel.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.io.*;
 import java.net.URI;
@@ -9,22 +11,84 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class LeaderboardPanel extends JPanel {
-    private final ArrayList<Score> leaderboard;
+    private List<Score> leaderboard;
     private final ArrayList<JLabel> scoreLabels;
     private final ArrayList<JLabel> nameLabels;
     private final GameDisplay display;
-    private final JButton menuButton, playButton;
+    private final JButton menuButton, playButton, clearButton, cleanButton;
+    private final JTable leaderboardTable;
+    private final JScrollPane scrollPane;
 
     public LeaderboardPanel(GameDisplay display) {
         this.display = display;
-        leaderboard = new ArrayList<>();
         scoreLabels = new ArrayList<>();
         nameLabels = new ArrayList<>();
+        leaderboard = new ArrayList<>();
+        leaderboardTable = new JTable(new TableModel() {
+            @Override
+            public int getRowCount() {
+                return leaderboard.size();
+            }
 
-        setLayout(new GridLayout(0,2));
+            @Override
+            public int getColumnCount() {
+                return 2;
+            }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+                if (columnIndex == 0) {
+                    return "score";
+                } else {
+                    return "name";
+                }
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex == 0) {
+                    return Integer.class;
+                } else {
+                    return String.class;
+                }
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                if (columnIndex == 0) {
+                    return leaderboard.get(rowIndex).getValue();
+                } else {
+                    return leaderboard.get(rowIndex).getName();
+                }
+            }
+
+            @Override
+            public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+            }
+
+            @Override
+            public void addTableModelListener(TableModelListener l) {
+
+            }
+
+            @Override
+            public void removeTableModelListener(TableModelListener l) {
+
+            }
+        });
+
+        setLayout(new BorderLayout());
 
         menuButton = new JButton("Menu");
         menuButton.addActionListener(e -> display.changePanel(GameDisplay.State.MENU));
@@ -32,11 +96,40 @@ public class LeaderboardPanel extends JPanel {
         playButton = new JButton("Again!");
         playButton.addActionListener(e -> display.changePanel(GameDisplay.State.DISPLAY));
 
-        add(menuButton);
-        add(playButton);
+        clearButton = new JButton("Clear scores");
+        clearButton.addActionListener(e -> {
+            leaderboard.clear();
+            leaderboardTable.repaint();
+        });
 
-        add(new JLabel("Score"));
-        add(new JLabel("Name"));
+        cleanButton = new JButton("Clean up scores (one per player)");
+        cleanButton.addActionListener(e -> {
+            updateArrays();
+            ArrayList<Score> unique = new ArrayList<>();
+            for (Score score : leaderboard) {
+                if (unique.stream().noneMatch(s -> s.getName().equals(score.getName()))) {
+                    unique.add(score);
+                }
+            }
+            leaderboard = unique;
+            leaderboardTable.repaint();
+        });
+
+        add(menuButton, BorderLayout.WEST);
+        add(playButton, BorderLayout.EAST);
+        add(cleanButton, BorderLayout.NORTH);
+        add(clearButton, BorderLayout.SOUTH);
+
+        scrollPane = new JScrollPane(leaderboardTable);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+        try (FileInputStream fileStream = new FileInputStream("src/src/scores.txt");
+             ObjectInputStream objectStream = new ObjectInputStream(fileStream)) {
+            leaderboard = (ArrayList<Score>) objectStream.readObject();
+        } catch (IOException | ClassNotFoundException ignored) {
+            leaderboard = new ArrayList<>();
+        }
 
         updateArrays();
         setVisible(true);
@@ -50,19 +143,7 @@ public class LeaderboardPanel extends JPanel {
     }
 
     private void updateArrays() {
-        Iterator<Score> iter = leaderboard.stream().sorted(Comparator.reverseOrder()).iterator();
-        int i = 0;
-        while (iter.hasNext()) {
-            Score score = iter.next();
-
-            scoreLabels.get(i).setText(Integer.toString(score.getValue()));
-            nameLabels.get(i).setText(score.getName());
-
-            add(scoreLabels.get(i));
-            add(nameLabels.get(i));
-
-            i++;
-        }
+        leaderboard = leaderboard.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public void addScore(Score score) {
@@ -78,7 +159,7 @@ public class LeaderboardPanel extends JPanel {
     }
 
     public void saveScores() {
-        try (FileOutputStream fileStream = new FileOutputStream("src/src/scores.tmp");
+        try (FileOutputStream fileStream = new FileOutputStream("src/src/scores.txt");
              ObjectOutputStream objectStream = new ObjectOutputStream(fileStream)) {
             objectStream.writeObject(leaderboard);
         } catch (IOException e) {
